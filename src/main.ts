2c63b17ed1e6ae6275503c65d90cd38f56cd9940
TNTSuperMan <abworld@outlook.jp>
2025-03-08 00:50:02 +0900

    typo

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 const g_reg = /^https?:\/\/(\\w+.)*(google|google-analytics|googletagmanager|chrome).(com|co\\.jp|net|org)\/?/;
 chrome.webRequest.onBeforeRequest.addListener(d=>({
     cancel: g_reg.test(d.url)
-}), {urls: ["<all_urls>"]}, ["blcking"])
+}), {urls: ["<all_urls>"]}, ["blocking"])
