2a5bdedd55a85724c2461daa6d80a39ec3bd311c
TNTSuperMan <abworld@outlook.jp>
2025-04-06 23:12:28 +0900

    ^^;

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
-const g_reg = /^https?:\/\/(\\w+.)*(google|google-analytics|googletagmanager|chrome).(com|co\\.jp|net|org)\/?/;
-chrome.webRequest.onBeforeRequest.addListener(d=>({
-    cancel: g_reg.test(d.url)
-}), {urls: ["<all_urls>"]}, ["blocking"])
+const g_reg = /^https?:\/\/(\w+\.)*[\w\d-]*(google|chrome|gstatic|youtube|ytimg)[\w\d-]*\.\w+\/?/;
+
+chrome.webRequest.onBeforeRequest.addListener((d) => {
+  if (g_reg.test(d.initiator ?? "") || g_reg.test(d.url)) {
+    console.log(`%cBLK ${d.url}`, "background:red");
+    return {
+        redirectUrl: "data:application/octet-stream,",
+        cancel: true
+    }
+  }
+  console.log(`%cGET ${d.url}`, "");
+}, { urls: ["*://*/*"] });
